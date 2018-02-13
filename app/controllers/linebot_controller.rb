e14abb5664556e63a36daedd2f1f5316b0e54e7a
Shoko KOBAYASHI <shoko.kobyashi@gmail.com>
2018-02-13 17:04:07 +0900

    add linebot_controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,34 +1,40 @@
 class LinebotController < ApplicationController
+  require 'line/bot'  # gem 'line-bot-api'
 
-	def callback
- 		body = request.body.read
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
 
-	 	signature = request.env['HTTP_X_LINE_SIGNATURE']
-		unless client.validate_signature(body, signature)
-			error 400 do 'Bad Request' end
-		end
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 
-		events = client.parse_events_from(body)
-  
-		events.each { |event|
-			case event
-			when Line::Bot::Event::Message
-				case event.type
-				when Line::Bot::Event::MessageType::Text
-					message = {
-						type: 'text',
-						text: event.message['text']
-					}
-					client.reply_message(event['replyToken'], message)
-				end
-			end
-		}
-	end
+  def callback
+    body = request.body.read
 
-	def client
-      @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      }
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
     end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+
+    head :ok
+  end
 end
